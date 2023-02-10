5976cae06075eebe5c4c13e4ffb1a10be66dd327
Andreas Stroehlein <astroehlein@unimelb.edu.au>
2023-02-10 18:15:43 +1100

    Plotting TODOs

--- frakka.py
+++ frakka.py
@@ -1,4 +1,5 @@
 from utils import *
+from plot import *
 import argparse
 import sys
 import re
---
+++ plot.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import sys
+
+def makePlotDir():
+	pass
+	# TODO check if dir exists
+	# TODO reate dir
+
+def plotCounts():
+
+	pass
+	# TODO Plot one figure for all species in the output list (sorted barplots side by side with median confidence score on top)
+	# TODO cut-off in title
+	# TODO name file according to input file name and confidence score cut-off
+	# TODO Merge cut-off (Put all below that cut-off together in Other)
+	# TODO Read count cut-off (drop the ones you that have < X reads)
+
+def plotConfDist():
+
+	pass
+	# TODO density plot, one for each species, confidence score on x axis density on y axis
+	# TODO Median as vertical line
+	# TODO Cut-off in title
+	# TODO file name in title
+	# TODO Species name in title
+	# TODO name file according to species + taxid + cut-off
